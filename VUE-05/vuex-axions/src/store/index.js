c17c0d13a58e324ee936c2b18bc9ec3150dcd5c4
Karen2310 <akarennavarrete@gmail.com>
2020-11-26 00:50:42 -0700

    axios

---
+++ VUE-05/vuex-axions/src/store/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    posts: [],
+    todos: []
+  },
+  mutations: {
+    SET_POSTS(state, posts) {
+      state.posts = posts
+    },
+    SET_TODOS(state, todos) {
+      state.todos = todos
+    }
+
+  },
+  getters: {
+    doneTodos: state => {
+      return state.todos.filter( todo=> todo.completed)
+    }
+  },
+  actions: {
+    getPosts({commit}){
+      axios.get('http://jsonplaceholder.typicode.com/posts')
+      .then(response=> {
+        console.log(response.data);
+        commit('SET_POSTS', response.data)
+      })
+      
+    },
+    getTodos({commit}){
+      axios.get('http://jsonplaceholder.typicode.com/todos')
+      .then(response => {
+        console.log(response.data);
+        commit('SET_TODOS', response.data)
+      })
+    }
+  },
+  modules: {
+  }
+})
