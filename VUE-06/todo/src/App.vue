7db3e1003134ab175afb734824346fc2c60438b4
Karen2310 <akarennavarrete@gmail.com>
2020-11-28 17:26:18 -0700

    Formulario -v-model

---
+++ VUE-06/todo/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div id="nav" class="navbar navbar-dark bg-dark">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view class="container"/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #fff;
+}
+
+#nav a.router-link-exact-active {
+  color: lightblue;
+}
+</style>
---
+++ VUE-06/todo/src/views/Home.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="home">
+  <h1 class="my-4">Agregar ToDo</h1>
+  <form @submit.prevent="guardarTodo">
+      <div class="form-group">
+      <label for="exampleInput1">Nombre</label>
+      <input v-model.trim="todo.nombre" type="text" class="form-control" id="exampleInput1" placeholder="Ingrese nombre tarea" >
+      </div>
+      <hr>
+      <div>Tipos</div>
+      <div class="form-check form-check-inline">
+        <input v-model="todo.tipos" class="form-check-input" type="checkbox" value="Hogar" id="defaultCheck1">
+        <label class="form-check-label" for="defaultCheck1">
+          Hogar
+        </label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input v-model="todo.tipos" class="form-check-input" type="checkbox" value="Escuela" id="defaultCheck2">
+        <label class="form-check-label" for="defaultCheck2">
+          Escuela
+        </label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input v-model="todo.tipos" class="form-check-input" type="checkbox" value="Oficina" id="defaultCheck3">
+        <label class="form-check-label" for="defaultCheck3">
+          Oficina
+        </label>
+      </div>
+      <hr>
+      <div>Prioridad</div>
+      <div class="form-check form-check-inline">
+        <input  v-model="todo.prioridad" class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="Baja">
+        <label class="form-check-label" for="inlineRadio1">Baja</label>
+      </div>
+      
+      <div class="form-check form-check-inline">
+        <input v-model="todo.prioridad" class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="Media">
+        <label class="form-check-label" for="inlineRadio2">Media</label>
+      </div>
+    
+      <div class="form-check form-check-inline">
+        <input v-model="todo.prioridad" class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="Alta">
+        <label class="form-check-label" for="inlineRadio3">Alta</label>
+      </div>
+      <hr>
+       <div class="form-group">
+      <label for="exampleInput2">Horas Requeridas</label>
+      <input v-model.number="todo.horas" type="number" class="form-control" id="exampleInput2" >
+      </div>
+
+      <button type="submit" class="btn btn-primary" :disabled="estadoBoton">Guardar</button>
+  </form>
+
+  </div>
+</template>
+
+<script>
+import { nanoid } from 'nanoid'
+import { mapActions} from 'vuex'
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }, 
+  data() {
+    return {
+      todo: {
+        id: '',
+        nombre:'',
+        tipos: [],
+        prioridad: '',
+        horas: 0
+      }
+    }
+  },
+  methods: {
+    ...mapActions(['setTodo']),
+    guardarTodo(){
+      console.log(this.todo);
+
+      this.todo.id = nanoid(5);
+
+      console.log(this.todo.id);
+      this.setTodo(this.todo);
+      this.limpiarTodo();
+    },
+    limpiarTodo(){
+      thid.todo={
+          id: '',
+        nombre:'',
+        tipos: [],
+        prioridad: '',
+        horas: 0
+      }
+    }
+  },
+  computed: {
+    estadoBoton(){
+      return this.todo.nombre.trim()==="" ? true : false
+    }
+  }
+}
+</script>
